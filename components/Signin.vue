b3cf05a94f41b27916e2cda9a597e2577373b31b
Xavier Munroe <munroexavier@gmail.com>
2021-01-30 19:42:34 -0500

    starting sign in for game

---
+++ components/Signin.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <form>
+      <h2>Please enter your name</h2>
+      <input type="text" @change="handleChange" :value="user" />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signin',
+  data: () => ({
+    user: '',
+  }),
+  methods: {
+    handleChange(e) {
+      this.user = e.target.value
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="container">
-
-  </div>
+  <div class="container"></div>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    user: '',
+  }),
+  mounted() {
+    const user = localStorage.getItem('compliments_user') || ''
+    this.user = user
+  },
+  updated() {
+    const user = localStorage.getItem('compliments_user') || ''
+    this.user = user
+  },
+  destroyed() {
+    localStorage.removeItem('compliments_user')
+  },
+}
 </script>
 
 <style>
-
 </style>
